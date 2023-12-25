afb7bc8d93e7877557e06a46f278c79a05f4752c
hugopereiira11 <hugopereiira11@gmail.com>
2023-12-24 22:36:14 -0300

    inicio da estrutura do projeto

---
+++ script.js
@@ -0,0 +1,19 @@
+const header = document.querySelector('header')
+const footer = document.querySelector('footer')
+
+// header.innerHTML += `
+//     <div id="logo">
+//       <h1>Base Prevenção 2024</h1>
+//     </div>
+//     <nav>
+//       <a href="nav/consumo/consumo.html">Consumo</a>
+//       <a href="#">Orcamentos</a>
+//       <a href="#">NF-e's de Devolução</a>
+//       <a href="#">Perdas</a>
+//     </nav>
+// `
+
+footer.innerHTML += `
+      <p>Acervo de Informaçoes do setor de <br> Prevenção de Perdas do MiniBox 31</p>
+
+`
